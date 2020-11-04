daef0e7f90fbceb9cba7e3303de7cfad7861752f
aland3v <aland3v@gmail.com>
2020-11-04 08:13:19 -0400

    Se termino el tema de los selectores

--- main2.js
+++ main2.js
@@ -34,3 +34,56 @@ $(document).ready(function(){
  */
 
 /** STYLING SPECIFIC CELLS */
+$(document).ready(function(){
+  //$('td:contains(Henry)').next().addClass('highlight')
+  $('td:contains(Henry)').nextAll().addClass('highlight')
+
+  // addBack(), selecciona todos los hermanos posteriores
+  // incluido el que hace match(el q contiene Hamlet)
+  $('td:contains(Hamlet)').nextAll().addBack().addClass('highlight')
+
+  // Muy descriptivo 
+  $('td:contains(Comedy)').parent().children().addClass('highlight')
+})
+
+/**
+ * next() -> selecciona al hermano inmediato
+ * nextAll() -> selecciona a todos los hermanos siguientes
+ */
+
+/**
+ * prev() -> contraparte de next
+ * prevAll() -> contraparte de nextAll
+ * .siblings() -> Selecciona todos los hermanos antes o despues
+ */
+
+
+/** ENCADENAMIENTO - CHAINING */
+/**
+ * How chaining works
+ * Almost all jQuery methods return a jQuery object and so
+ * can have more jQuery methods applied to the result.
+ */
+
+ /**
+  * Every selector expression and most jQuery methods
+  * return a jQuery object.
+  * This is almost always what we want, because of the
+  * implicit iteration and chaining capabilities that it affords.
+  */
+/** Multiples lineas para una mejor lectura, ejemplo de encadenamiento */
+$(document).ready(function(){
+  $('td:contains(Henry)').parent().find('td:eq(1)')
+      .addClass('highlight').end().find('td:eq(2)')
+                            .addClass('highlight')
+})
+
+$(document).ready(function(){
+  $('td:contains(Henry)')
+      .parent()
+      .find('td:eq(1)')
+      .addClass('highlight')
+      .end()
+      .find('td:eq(2)')
+      .addClass('highlight')
+})

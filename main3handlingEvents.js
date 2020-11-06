a55e73d418b95a58d5232121e1e351b7e7cf6530
aland3v <aland3v@gmail.com>
2020-11-05 20:11:02 -0400

    solo pequeños apuntes

---
+++ main3handlingEvents.js
@@ -0,0 +1,7 @@
+/**Hay dos formas de esperar que la pagina cargue
+ * $(document).ready().- esta disponible antes, pero a veces querremos usar onload
+ * se recomienda definir todos los links, styles en la cabezera (HEAD) y al final jquery(al Pie)
+ * window.onload .-  espera a q cargue TODO, imgs, files,....
+*/
+
+/* Handling multiple scripts on one page */
